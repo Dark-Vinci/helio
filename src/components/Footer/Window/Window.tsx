b66d35de5397ee60e7bb4d5c7024aabcb96db9d9
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-23 22:23:12 +0100

    chore: work no dey finish

--- src/components/Footer/Window/Window.tsx
+++ src/components/Footer/Window/Window.tsx
@@ -2,10 +2,69 @@ import { JSX } from 'react';
 
 import style from './Window.module.scss';
 
-export function Window(): JSX.Element {
+interface WindowProps {
+    readonly title: string;
+    readonly isOpened: boolean;
+    readonly description: string[];
+    readonly list: string[];
+}
+
+export function Window({
+    title, 
+    isOpened, 
+    description, 
+    list 
+}: WindowProps): JSX.Element {
     return (
         <div className={ style.container }>
-            window
+            <div className={ style.window_container }>
+                <div className={ style.title }>
+                    <p>{ title }</p>
+                    <div className="icon">
+                        ICON
+                    </div>
+                </div>
+
+                <div 
+                    className={ style.body }
+                    style={{ display: isOpened ? 'auto' : 'none'}}
+                >
+                    {/* list of descriptions */}
+                    <div className={ style.description }>
+                        {
+                            description.map((el, i) => {
+                                return (
+                                    <div
+                                        key={i}
+                                    >
+                                        <p>{ el }</p>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+
+                    {/* list of items */}
+                    {
+                        list.length > 0 ?
+                            <ul className={ style.list }>
+                                {
+                                    list.map((el, i) => {
+                                        return (
+                                            <li
+                                                key={i}
+                                            >
+                                                { el }
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+                            :
+                            null
+                    }
+                </div>
+            </div>
         </div>
     );
 }
