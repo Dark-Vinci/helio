df3dfe1726ee9c7e793b3cac90746d8108ce1e37
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-23 08:03:31 +0100

    what is wrong

---
+++ commit.sh
@@ -0,0 +1,11 @@
+
+echo '🤤ADDING FILES TO STAGING🤤'
+git add .
+
+echo '🤞👏FILES WILL GET COMMITED🤞👏'
+git commit -am $1
+
+echo 'PUSHING TO GITHUB'
+# git push -u origin main
+
+echo '🚀🚀🚀PUSHED TO GITHUB🚀🚀🚀'
