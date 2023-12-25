166f6e91186d00bb34ab3596a8402c38f2cb5570
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-25 03:32:15 +0100

    chore: were done with the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,32 @@ import style from './Header.module.scss';
 export function Header(): JSX.Element {
     return (
         <div className={ style.container }>
-            HEADER
+            <div className={ style.header_container }>
+                <video
+                    loop
+                    muted
+                    autoPlay
+                >
+                    <source 
+                        src="https://helio.money/static/animations/bg-desktop.mp4" 
+                        type="video/mp4" 
+                    />
+                </video>
+                <div className={ style.other_container }>
+                    <div className={ style.content_container }>
+                        <div className={ style.content }>
+                            <div className={ style.word }>
+                                <p>Helio Protocol</p>
+                                <p>The revolutionary USD Destablecoin backed by BNB</p>
+                            </div>
+                            <div className={ style.button }>
+                                <button>Start Earning</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={ style.bot }></div>
+                </div>
+            </div>
         </div>
     );
 }
