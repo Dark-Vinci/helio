18d22b5b0f025867b9202501af457f41650b1ebb
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-25 15:42:30 +0100

    chore: more update

--- src/components/Body/FlipCard/FlipCard.tsx
+++ src/components/Body/FlipCard/FlipCard.tsx
@@ -1,11 +1,43 @@
 import { JSX }  from 'react';
+
 import style from './FlipCard.module.scss';
 
+interface FlipCardProps {
+    readonly backs: string[];
+    readonly title: string;
+    readonly url: string;
+}
 
-export function FlipCard(): JSX.Element {
+export function FlipCard({
+    url, 
+    backs, 
+    title
+}: FlipCardProps): JSX.Element {
+
     return (
         <div className={ style.container }>
-            
+            <div className={ style.front }>
+                <img 
+                    src={ url } 
+                    alt="url" 
+                />
+                <p>{ title }</p>
+            </div>
+            <div className={ style.back }>
+                <ul>
+                    {
+                        backs.map((el, i) => {
+                            return (
+                                <li
+                                    key={i}
+                                >
+                                    { el }
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     );
 }
