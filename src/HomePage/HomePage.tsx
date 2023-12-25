98b106c8d848e6ad7262beca160ddb13751b0d9a
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-25 16:56:27 +0100

    chore: done already

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,13 +1,28 @@
-import { JSX } from 'react';
+import { JSX, useState, useEffect } from 'react';
 
 import style from './HomePage.module.scss'
 import { Body, Footer, Header, Nav } from '@components';
 
 export function HomePage(): JSX.Element {
+    const [scrollHeight, setScrollHeight] = useState<number>(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const newHeight = window.scrollY;
+            setScrollHeight(newHeight);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
         <div className={ style.container }>
             <nav>
-                <Nav />
+                <Nav scrollHeight={scrollHeight}/>
             </nav>
 
             <body>
